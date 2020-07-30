b687b2604ca09f639285368c13ca893e0652748f
Erick Montero <atlhasdemon@hotmail.com>
2020-07-30 18:11:35 -0500

    Update login.php

--- login.php
+++ login.php
@@ -18,21 +18,27 @@
     <body id="page-top">
         
         <!-- Masthead-->
-        <header class="masthead">
-            <div class="container d-flex h-100 align-items-center">
-                <div class="mx-auto text-center">
-                    <h1 class="mx-auto my-0 text-uppercase">Grayscale</h1>
-                    <h2 class="text-white-50 mx-auto mt-2 mb-5">A free, responsive, one page Bootstrap theme created by Start Bootstrap.</h2>
-                    <a class="btn btn-primary js-scroll-trigger" href="#about">Get Started</a>
+        <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
+            <div class="container">
+                <a class="navbar-brand js-scroll-trigger" href="#page-top">Base de datos Videojuegos</a>
+                <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                    Menu
+                    <i class="fas fa-bars"></i>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#lista">Login</a></li>
+                        <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#insertar">Sin Up</a></li>
+                    </ul>
                 </div>
             </div>
-        </header>
-        <!-- About-->
-        <section class="about-section text-center" id="about">
+        </nav>
+        <!-- Login-->
+        <section class="about-section text-center" id="container">
             <div class="container">
             <form action="" method="post">
             <h3>Iniciar Secion</h3>
-            <img src="./img/login.png" alt="login" width="100" height="100">
+            <img src="./assets/img/user.png" alt="login" width="100" height="100">
             <input type="text" name="usuario" placeholder="Usuario">
             <input type="password" name="clave" placeholder="Contraseña">
             <!-- con eso imprimimos si alert no esta vacio -->
